0b9fbade356d0612dc0048bc2fac90870d7ea724
Efim Zbarasky <zzfima@gmail.com>
2022-07-29 18:57:40 +0300

    rfct

--- db/migrate/20220729154753_create_questions.rb
+++ db/migrate/20220729154753_create_questions.rb
@@ -2,8 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
       t.text :body
-      t.integer :user_id
-
+      t.integer :user_id, index: true
       t.timestamps
     end
   end
